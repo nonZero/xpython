6b6864fda068b1100544214626f59c1e55f7ed16
Udi Oron <udioron@gmail.com>
2016-11-09 23:19:38 +0200

    clock: Nicer reference solution
    where Clock is immutable and better code reuse.

--- exercises/clock/example.py
+++ exercises/clock/example.py
@@ -1,24 +1,13 @@
-
 class Clock:
-    'Clock that displays 24 hour clock that rollsover properly'
+    def __init__(self, hour, minutes):
+        self.hour = (hour + minutes // 60) % 24
+        self.minutes = minutes % 60
 
-    def __init__(self, hour, minute):
-        self.hour = hour
-        self.minute = minute
-        self.cleanup()
-
-    def __repr__(self):
-        return "%02d:%02d" % (self.hour, self.minute)
+    def __str__(self):
+        return "{:02}:{:02}".format(self.hour, self.minutes)
 
     def __eq__(self, other):
-        return repr(self) == repr(other)
+        return self.hour == other.hour and self.minutes == other.minutes
 
     def add(self, minutes):
-        self.minute += minutes
-        return self.cleanup()
-
-    def cleanup(self):
-        self.hour += self.minute // 60
-        self.hour %= 24
-        self.minute %= 60
-        return self
+        return Clock(self.hour, self.minutes + minutes)
